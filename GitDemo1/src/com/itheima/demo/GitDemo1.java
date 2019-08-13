39c8bdb8ac49ba84f6e012fbf5ea5f70e35249e6
zhangsan <1656726111@qq.com>
2019-08-13 14:58:27 +0800

    demo

---
+++ GitDemo1/src/com/itheima/demo/GitDemo1.java
@@ -0,0 +1,10 @@
+package com.itheima.demo;
+
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
